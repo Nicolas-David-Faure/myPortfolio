74ace71f10174266ccd31a30f6d33f3ca66b5943
Nicolás Faure <nfaure@insideone.cloud>
2024-03-07 12:28:38 -0300

    animation not

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -73,127 +73,32 @@ const projectsData = [
 ];
 
 const ProjectsList = ({ language, project }) => {
-  const [mauseEnterState, setMauseEnterState] = useState(false);
-  const [toggleProject, setToggleProject] = useState(false);
-  const handleMouseEnter = (e) => {
-    setMauseEnterState(true);
-  };
-  const handleMouseLeave = (e) => {
-    setMauseEnterState(false);
-  };
-
-  const handleToggleProject = () => setToggleProject(!toggleProject);
-
-  const arrowVariants = {
-    on: { rotate: 90 },
-    off: { rotate: 0 },
-  };
-
-  const descriptionVariants = {
-    on: { height: "auto", opacity: 1 },
-    off: { height: 0, opacity: 0 },
-  };
-
-  const projectsVariants = {
-    on: { opacity: [0, 0, 0.4, 1], x: 0, y: 0, zIndex: 1 },
-    off: { opacity: 0, zIndex: -1, y: -50 },
-  };
-
   return (
     <li className="projectsList__item">
       <div className="projectsList__item__content">
-        <motion.div
-          initial="off"
-          animate={toggleProject ? "on" : "off"}
-          variants={arrowVariants}
-          className="projectsList__item__icon"
-        >
-          <RightArrowIcon
-            style={{ ...iconDefaultStyles, width: 20, height: 20 }}
-          />
-        </motion.div>
-
+     
         <div className="projectsList__container__title">
-          <motion.h2
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            onClick={handleToggleProject}
-          >
-            {project.title}
-          </motion.h2>
-          <UnderlineAnimation
-            toggleProject={toggleProject}
-            onHoverMauseState={mauseEnterState}
-            color="#3498db"
-            height={2}
-          />
+          <motion.h2>{project.title}</motion.h2>
         </div>
       </div>
 
-      <motion.div
-        initial="off"
-        animate={toggleProject ? "on" : "off"}
-        variants={descriptionVariants}
-        transition={{ duration: 0.5 }}
-        className="projectsList__item__description"
-      >
-        {toggleProject && (
-          <>
-            <AnimatePresence>
-              <motion.figcaption
-                initial={{ opacity: 0, y: -50, zIndex: -1 }}
-                animate={toggleProject ? "on" : "off"}
-                variants={projectsVariants}
-                transition={{
-                  duration: toggleProject ? 1 : 0.1,
-                  delay: 0.1,
-                  type: "spring",
-                  stiffness: 50,
-                }}
-              >
-                <img src={project.img} alt={project.title} />
-              </motion.figcaption>
+      <motion.div className="projectsList__item__description">
+        <motion.figcaption>
+          <img src={project.img} alt={project.title} />
+        </motion.figcaption>
 
-              <motion.div
-                initial={{ opacity: 0, x: 0, y: -50, zIndex: -1 }}
-                animate={toggleProject ? "on" : "off"}
-                variants={projectsVariants}
-                transition={{
-                  duration: toggleProject ? 1 : 0.1,
+        <motion.div className="projectsList__item__description__container">
+          <div className="projectsList__item__description__text">
+            <strong>{project.description[language]}</strong>
+          </div>
 
-                  delay: 0.6,
-                  type: "spring",
-                  stiffness: 50,
-                }}
-                className="projectsList__item__description__container"
-              >
-                <div className="projectsList__item__description__text">
-                  <strong>{project.description[language]}</strong>
-                </div>
-
-                <motion.div
-                  initial={{ opacity: 0, zIndex: -1, y: -50 }}
-                  animate={toggleProject ? "on" : "off"}
-                  variants={projectsVariants}
-                  transition={{
-                    duration: toggleProject ? 1 : 0.1,
-                    delay: 1.2,
-                    type: "spring",
-                    stiffness: 50,
-                  }}
-                  className="projectsList__item__description__footer"
-                >
-                  <button>
-                    {language === "en" ? "View Project" : "Ver Proyecto"}
-                  </button>
-                  <button>
-                    {language === "en" ? "View Code" : "Ver Código"}
-                  </button>
-                </motion.div>
-              </motion.div>
-            </AnimatePresence>
-          </>
-        )}
+          <motion.div className="projectsList__item__description__footer">
+            <button>
+              {language === "en" ? "View Project" : "Ver Proyecto"}
+            </button>
+            <button>{language === "en" ? "View Code" : "Ver Código"}</button>
+          </motion.div>
+        </motion.div>
       </motion.div>
     </li>
   );
@@ -203,10 +108,6 @@ export const Projects = ({ language }) => {
   return (
     <div className="projects__main">
       <div className="projects__container">
-        <div className="projects__header">
-          <h1>{language === "en" ? "Projects" : "Proyectos"}</h1>
-        </div>
-
         <ul className="projectsList__main">
           {projectsData.map((project, index) => {
             return (
