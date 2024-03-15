b13272d795440a35d78a81a46a7f78ac6ae0b288
Nicolás Faure <nfaure@insideone.cloud>
2024-03-14 22:08:26 -0300

    new form to show services

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./aboutme.scss";
-import backEndIcon from "../../assets/img/servidor.png";
+import teamworkIcon from "../../assets/img/teamwork.png";
+
 import frontEndIcon from "../../assets/img/front.png";
 import dbIcon from "../../assets/img/db.png";
+import servicesInformation from '../../mooks/servicesInformation.json'
+
+
 //logos skills
 //front
 import cssLogo from "../../assets/img/logo-skills/CSS3.png";
@@ -31,89 +35,44 @@ import githubLogo from "../../assets/img/logo-skills/Github.png";
 import dockerLogo from "../../assets/img/logo-skills/docker.png";
 
 const arrayLogos = [
-      { name: "CSS", logo: cssLogo },
-      { name: "HTML", logo: htmlLogo },
-      { name: "Javascript", logo: javascriptLogo },
-      { name: "React", logo: reactLogo },
-      { name: "Next.js", logo: nextLogo },
-      { name: "Redux", logo: reduxLogo },
-      { name: "Sass", logo: sassLogo },
-      { name: "Tailwind", logo: tailwindLogo },
-      { name: "Typescript", logo: typescriptLogo },
-      { name: "Express", logo: expressLogo },
-      { name: "Firebase", logo: firebaseLogo },
-      { name: "MongoDB", logo: mongodbLogo },
-      { name: "Node.js", logo: nodeLogo },
-      { name: "Postgres", logo: postgresLogo },
-      { name: "Nest", logo: nestLogo },
-      { name: "OpenAI", logo: openaiLogo },
-      { name: "JWT", logo: jwtLogo },
-      { name: "Typescript", logo: tsLogo },
-      { name: "Git", logo: gitLogo },
-      { name: "Github", logo: githubLogo },
-      { name: "Docker", logo: dockerLogo },
-    ]
- 
+  { name: "CSS", logo: cssLogo },
+  { name: "HTML", logo: htmlLogo },
+  { name: "Javascript", logo: javascriptLogo },
+  { name: "React", logo: reactLogo },
+  { name: "Next.js", logo: nextLogo },
+  { name: "Redux", logo: reduxLogo },
+  { name: "Sass", logo: sassLogo },
+  { name: "Tailwind", logo: tailwindLogo },
+  { name: "Typescript", logo: typescriptLogo },
+  { name: "Express", logo: expressLogo },
+  { name: "Firebase", logo: firebaseLogo },
+  { name: "MongoDB", logo: mongodbLogo },
+  { name: "Node.js", logo: nodeLogo },
+  { name: "Postgres", logo: postgresLogo },
+  { name: "Nest", logo: nestLogo },
+  { name: "OpenAI", logo: openaiLogo },
+  { name: "JWT", logo: jwtLogo },
+  { name: "Typescript", logo: tsLogo },
+  { name: "Git", logo: gitLogo },
+  { name: "Github", logo: githubLogo },
+  { name: "Docker", logo: dockerLogo },
+];
+
 export const Aboutme = ({ language }) => {
   return (
     <section className="aboutme__main">
-
       <div className="aboutme__container">
-      {/* description */}
-      <div className="aboutme__description">
+        {/* description */}
+        {/* <div className="aboutme__description">
         <strong>
           {language === "en"
             ? "I am a full stack web developer, with experience in web applications development. I love learning new technologies and applying them in my projects. I love working in a team and collaborating with other developers."
             : "Soy un desarrollador web full stack, con experiencia en el desarrollo de aplicaciones web . Me encanta aprender nuevas tecnologías y aplicarlas en mis proyectos. Me encanta trabajar en equipo y colaborar con otros desarrolladores."}
         </strong>
+      </div> */}
+
+        <AboutmeServices language={language} />
       </div>
-    {/* services */}
-      <div className="aboutme__services">
-        <div className="aboutme__services__list">
-          <div className="aboutme__service">
-            <figure className="aboutme__service_img">
-              <img src={frontEndIcon} />
-            </figure>
-
-            <p>
-              {language === "en"
-                ? "I develop web applications using react and next.js"
-                : "Desarrollo aplicaciones web en react y next.js"}
-            </p>
-          </div>
-
-          <div className="aboutme__service">
-            <figure className="aboutme__service_img">
-              <img src={dbIcon} />
-            </figure>
-
-            <p>
-              {language === "en"
-                ? "I design and develop databases using Postgres, MongoDB and Firebase."
-                : "Diseño y desarrollo bases de datos utilizando Postgres, MongoDB y Firebase."}
-            </p>
-          </div>
-
-          <div className="aboutme__service">
-            <figure className="aboutme__service_img">
-              <img src={backEndIcon} />
-            </figure>
-
-            <p>
-              {language === "en"
-                ? "I develop server-side applications using Node.js and Express.js."
-                : "Desarrollo aplicaciones del lado del servidor utilizando Node.js y Express.js."}
-            </p>
-          </div>
-        </div>
-      </div>
-
-      </div>
-
-      
-
-
-
 
       <div className="aboutme__skills">
         <h2>{language === "en" ? "Skills" : "Habilidades"}</h2>
@@ -126,8 +85,70 @@ export const Aboutme = ({ language }) => {
           ))}
         </div>
       </div>
-
-
     </section>
+  );
+};
+
+const AboutmeServices = ({ language }) => {
+ 
+ console.log(servicesInformation)
+
+  return (
+    <div className="aboutme__services">
+      <div className="aboutme__services__list">
+        <div className="aboutme__service">
+          <figure className="aboutme__service_img">
+            <img src={frontEndIcon} />
+            <figcaption>
+              <p>Front end</p>
+            </figcaption>
+          </figure>
+
+         <ul>
+            {servicesInformation[language].frontend.map((service, i) => (
+              <li key={i}>
+                {service}
+              </li>
+            ))}
+         </ul>
+        </div>
+
+        <div className="aboutme__service">
+          <figure className="aboutme__service_img">
+            <img src={dbIcon} />
+
+            <figcaption>
+              <p>Back end</p>
+            </figcaption>
+          </figure>
+
+          <ul>
+            {servicesInformation[language].backend.map((service, i) => (
+              <li key={i}>
+                {service}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="aboutme__service">
+          <figure className="aboutme__service_img">
+            <img src={teamworkIcon} />
+            <figcaption>
+              {" "}
+              <p>{language === "en" ? 'Team work' : 'trabajo en equipo'}</p>
+            </figcaption>
+          </figure>
+
+         <ul>
+            {servicesInformation[language].teamwork.map((service, i) => (
+              <li key={i}>
+                {service}
+              </li>
+            ))}
+         </ul>
+        </div>
+      </div>
+    </div>
   );
 };
