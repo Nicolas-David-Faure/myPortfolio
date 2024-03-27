c6da4166dea71e4b80a80a27c01b12054cc87ab3
Nicolás Faure <nfaure@insideone.cloud>
2024-03-26 21:46:11 -0300

    contact add

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import './contact.scss'
+import React from "react";
+import "./styles/contacto.scss";
 
-export const Contact = ({language}) => {
+export const Contact = ({ language }) => {
   return (
-    <div className='contact__main'>
-      <h1>{language === 'es' ? 'Contacto' : 'Contact'}</h1>
-    
-      
-    </div>
-  )
-}
+    <section id="contact" className="contacto__container">
+      <div  className="contacto__title">
+
+        <h3>{language === "en" ? "contact" : "contacto"}</h3>
+
+      </div>
+
+      <div className="contacto__divisor">
+        <form className="contacto__form">
+          <div className="form__cont_label">
+            <label className="form__label" for="form__name">
+              Name:
+            </label>
+          </div>
+
+          <input
+            className="form__inputs"
+            placeholder="Ex: Jhon Smit"
+            id="form__name"
+            type="text"
+          />
+          <div className="form__cont_label">
+            <label className="form__label" for="form__email">
+              Email:
+            </label>
+          </div>
+          <input
+            name="email"
+            placeholder="Ex: any@any.com"
+            className="form__inputs"
+            id="form__email"
+            type="email"
+          />
+
+          <textarea
+            className="form__textArea"
+            name="mensaje"
+            id=""
+            cols="25"
+            rows="4"
+          ></textarea>
+
+          <button className="form__btn" type="submit">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
